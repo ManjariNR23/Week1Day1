6e8833c4b204fb584a25910691fac07152cf3a11
ManjariNR23 <110730038+ManjariNR23@users.noreply.github.com>
2022-08-06 19:23:16 +0530

    Add files via upload
    Day 1 Assignments

---
+++ Report.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Students details = new Students();
+       System.out.println(details.studentName);
+       System.out.println(details.initial);
+       System.out.println(details.rollNo);
+       System.out.println(details.collegeName);
+       System.out.println(details.markScored);
+       System.out.println(details.cgpa);
+	}
+
+}
---
+++ Students.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Students {
+
+	String studentName = "Manjari";
+	char initial = 'N';
+	int rollNo = 111;
+	String collegeName = "SRM University";
+	int markScored = 550;
+	float cgpa = 8.5f;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
